65dd30d2b9bc2639752074ebbc42de0635b16bed
khasakinil <khasakinil@gmail.com>
2024-03-10 23:19:15 +0530

    added problem statement

--- ProblemSolvingGeneric/src/com/dsa/generic/RemoveDuplicatesFromSortedArray.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,13 @@
 package com.dsa.generic;
 
+/*
+ * Remove Duplicates from Sorted Array
+ * Given an integer array nums sorted in non-decreasing order, 
+ * remove the duplicates in-place such that each unique element appears only once. 
+ * The relative order of the elements should be kept the same. 
+ * Then return the number of unique elements in nums.
+ */
+
 public class RemoveDuplicatesFromSortedArray {
 	public static void main(String args[]) {
 		RemoveDuplicatesFromSortedArray remDup = new RemoveDuplicatesFromSortedArray();
