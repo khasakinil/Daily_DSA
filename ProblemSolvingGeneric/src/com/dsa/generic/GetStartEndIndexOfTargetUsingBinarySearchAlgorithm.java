3a23abd5180a1142251d5f9e13b35a406e05aae1
khasakinil <khasakinil@gmail.com>
2024-03-14 19:35:43 +0530

    comment added

--- ProblemSolvingGeneric/src/com/dsa/generic/GetStartEndIndexOfTargetUsingBinarySearchAlgorithm.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/GetStartEndIndexOfTargetUsingBinarySearchAlgorithm.java
@@ -4,6 +4,7 @@ public class GetStartEndIndexOfTargetUsingBinarySearchAlgorithm {
 	public static void main(String[] args) {
 		int[] arr = { 1, 2, 2, 3, 3, 4, 4, 4, 5, 6 };
 		int target = 3;
+		//return [-1, -1] if target not found
 		int[] targetOutput = getTarget(arr, target);
 		for (int i = 0; i < targetOutput.length; i++) {
 			System.out.print(targetOutput[i] + ", ");
