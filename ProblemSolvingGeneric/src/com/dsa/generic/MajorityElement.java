34cb3f79b0eecfa281e1798228dd096aa98a6573
khasakinil <khasakinil@gmail.com>
2024-03-16 19:28:06 +0530

    get majority element from the array

---
+++ ProblemSolvingGeneric/src/com/dsa/generic/MajorityElement.java
@@ -0,0 +1,34 @@
+package com.dsa.generic;
+
+import java.util.HashMap;
+import java.util.Map;
+/*
+ * leetcode : 169. Majority Element
+ * Given an array nums of size n, return the majority element.
+ * The majority element is the element that appears more than [n / 2] times. 
+ * You may assume that the majority element always exists in the array.
+ */
+public class MajorityElement {
+	public static void main(String args[]) {
+		int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+		int majorityElement = getMejorityElement(nums);
+		System.out.println("Majority Element : " + majorityElement);
+	}
+
+	private static int getMejorityElement(int[] nums) {
+		Map<Integer, Integer> majorityMap = new HashMap<>();
+		int majorityValue = 0;
+		int majorityElement = 0;
+		for (int i = 0; i < nums.length; i++) {
+			majorityMap.put(nums[i], majorityMap.getOrDefault(nums[i], 0) + 1);
+		}
+
+		for (Map.Entry<Integer, Integer> entry : majorityMap.entrySet()) {
+			if ((int) entry.getValue() > majorityValue) {
+				majorityValue = (int) entry.getValue();
+				majorityElement = (int) entry.getKey();
+			}
+		}
+		return majorityElement;
+	}
+}
