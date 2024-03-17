d74d59892cddfc09fd26a13afa1088762e1b5691
khasakinil <khasakinil@gmail.com>
2024-03-17 23:36:36 +0530

    rotate array using extra arr = space O(n)

--- ProblemSolvingGeneric/src/com/dsa/generic/RotateArray.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/RotateArray.java
@@ -5,19 +5,21 @@ public class RotateArray {
 		int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
 		int k = 3;
 
-		getRotatedArray(nums, k);
+		int[] rotatedArr = getRotatedArray(nums, k + 1);
 		System.out.println("Rotated Array : ");
-		for (int i = 0; i < nums.length; i++) {
-			System.out.print(nums[i] + " ");
+		for (int i = 0; i < rotatedArr.length; i++) {
+			System.out.print(rotatedArr[i] + " ");
 		}
 	}
 
-	private static void getRotatedArray(int[] nums, int k) {
+	private static int[] getRotatedArray(int[] nums, int k) {
+		int[] rotateArr = new int[nums.length];
 		for (int i = 0; i < nums.length; i++) {
-			int temp = (i + k) > nums.length - 1 ? i : i + k;
-			int swapTemp = nums[temp];
-			nums[temp] = nums[i];
-			nums[i] = swapTemp;
+			if (k >= nums.length) {
+				k = 0;
+			}
+			rotateArr[k++] = nums[i];
 		}
+		return rotateArr;
 	}
 }
