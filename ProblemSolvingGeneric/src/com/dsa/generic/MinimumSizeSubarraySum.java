2310aeea47c08ef6b68ab119f50cba8c26c39248
khasakinil <khasakinil@gmail.com>
2024-03-29 14:35:49 +0530

    description added

--- ProblemSolvingGeneric/src/com/dsa/generic/MinimumSizeSubarraySum.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/MinimumSizeSubarraySum.java
@@ -4,6 +4,25 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
 
+/*
+ * Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+ * subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+ * 
+ * Example 1:
+ * Input: target = 7, nums = [2,3,1,2,4,3]
+ * Output: 2
+ * Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+ * 
+ * Example 2:
+ * Input: target = 4, nums = [1,4,4]
+ * Output: 1
+ * 
+ * Example 3:
+ * Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+ * Output: 0
+ * 
+ */
+
 public class MinimumSizeSubarraySum {
 
 	public static void main(String[] args) {
@@ -31,6 +50,6 @@ public class MinimumSizeSubarraySum {
 				}
 			}
 		}
-		return localTarget>=target?requiredElement : 0;
+		return localTarget >= target ? requiredElement : 0;
 	}
 }
