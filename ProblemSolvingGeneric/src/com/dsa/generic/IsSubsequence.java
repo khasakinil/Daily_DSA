703c103714f92639117bb406d7d6fad07d3a59c4
khasakinil <khasakinil@gmail.com>
2024-04-01 10:06:42 +0530

    corner check for sub str length zero

--- ProblemSolvingGeneric/src/com/dsa/generic/IsSubsequence.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/IsSubsequence.java
@@ -16,6 +16,11 @@ public class IsSubsequence {
 	}
 
 	private static boolean isSubsequence(String s, String t) {
+
+		if (s.length() == 0) {
+			return true;
+		}
+
 		int i = 0;
 		int j = 0;
 		while (i < s.length() && j < t.length()) {
