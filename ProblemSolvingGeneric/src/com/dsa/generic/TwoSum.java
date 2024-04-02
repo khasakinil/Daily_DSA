d2122d170867765787755120096229704947aa48
khasakinil <khasakinil@gmail.com>
2024-04-02 12:42:17 +0530

    two sum final solution

--- ProblemSolvingGeneric/src/com/dsa/generic/TwoSum.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/TwoSum.java
@@ -3,11 +3,28 @@ package com.dsa.generic;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
+ * find two numbers such that they add up to a specific target number. 
+ * Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+ * 
+ * Example 1:
+ * Input: numbers = [2,7,11,15], target = 9
+ * Output: [1,2]
+ * Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+ * 
+ * Example 2:
+ * Input: numbers = [2,3,4], target = 6
+ * Output: [1,3]
+ * Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+ * 
+ */
+
 public class TwoSum {
 
 	public static void main(String[] args) {
-		int[] numbers = { -1,0 };
-		int target = -1;
+		int[] numbers = { 0, 0, 5, 7 };
+		int target = 0;
 		int[] twoSumIndex = getTwoSumIndex(numbers, target);
 		System.out.println("twoSumIndex : ");
 		for (int i = 0; i < twoSumIndex.length; i++) {
@@ -23,7 +40,22 @@ public class TwoSum {
 
 		for (Map.Entry entry : numMap.entrySet()) {
 			if (numMap.containsKey(entry.getKey()) && numMap.containsKey(target - (int) entry.getKey())) {
-				int out[] = { (int) entry.getValue(), numMap.get(target - (int) entry.getKey()) };
+				int firstElement = (int) entry.getValue();
+				if ((int) entry.getValue() == numMap.get(target - (int) entry.getKey())) {
+					for (int i = 0; i < numbers.length; i++) {
+						if ((int) entry.getKey() == numbers[i]) {
+							firstElement = i + 1;
+							break;
+						}
+					}
+				}
+
+				int out[] = {
+						firstElement < numMap.get(target - (int) entry.getKey()) ? firstElement
+								: numMap.get(target - (int) entry.getKey()),
+						numMap.get(target - (int) entry.getKey()) > firstElement
+								? numMap.get(target - (int) entry.getKey())
+								: firstElement };
 				return out;
 			}
 		}
