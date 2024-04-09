146070dbb922aa34ee7f4c3b714ca6e78d1f1915
khasakinil <khasakinil@gmail.com>
2024-04-09 12:19:57 +0530

    alignment

--- ProblemSolvingGeneric/src/com/dsa/generic/LongestCommonPrefix.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/LongestCommonPrefix.java
@@ -10,15 +10,19 @@ public class LongestCommonPrefix {
 	public static String longestCommonPrefix(String[] strs) {
 		String commonPrefix = "";
 		int j = 0;
-		while (true) {
+		if(strs.length == 0) {
+			commonPrefix = strs[0];
+		}
+		while (true && strs.length>1) {
 			boolean needToBreak = false;
 			for (int i = 1; i < strs.length; i++) {
-				if (j >= strs[i].length() || strs[i - 1].charAt(j) != strs[i].charAt(j)) {
-					needToBreak = true;
-					break;
+				if (strs[i].length() > 0 && strs[i - 1].length() > 0) {
+					if (j >= strs[i].length() || strs[i - 1].charAt(j) != strs[i].charAt(j)) {
+						needToBreak = true;
+						break;
+					}
 				}
 			}
-
 			if (needToBreak) {
 				break;
 			} else {
