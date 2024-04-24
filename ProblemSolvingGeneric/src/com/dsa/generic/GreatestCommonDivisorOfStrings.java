d5ca92f6623b1c5ccd9d59d0397e8223a4d1bf84
khasakinil <khasakinil@gmail.com>
2024-04-24 22:16:18 +0530

    GreatestCommonDivisorOfStrings

--- ProblemSolvingGeneric/src/com/dsa/generic/GreatestCommonDivisorOfStrings.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/GreatestCommonDivisorOfStrings.java
@@ -3,13 +3,25 @@ package com.dsa.generic;
 public class GreatestCommonDivisorOfStrings {
 
 	public static void main(String[] args) {
-		String str1 = "ABCABC", str2 = "ABC";
+		String str1 = "ABABAB", str2 = "ABAB";
 		String str = getGreatestCommonDivisorOfStrings(str1, str2);
 		System.out.println("GreatestCommonDivisorOfStrings : " + str);
 	}
 
 	private static String getGreatestCommonDivisorOfStrings(String str1, String str2) {
-		return null;
+		if (str1.length() < str2.length()) {
+			return getGreatestCommonDivisorOfStrings(str2, str1);
+		}
+
+		else if (!str1.startsWith(str2)) {
+			return "";
+		}
+
+		else if (str2.isEmpty()) {
+			return str1;
+		} else {
+			return getGreatestCommonDivisorOfStrings(str1.substring(str2.length()), str2);
+		}
 	}
 
 }
