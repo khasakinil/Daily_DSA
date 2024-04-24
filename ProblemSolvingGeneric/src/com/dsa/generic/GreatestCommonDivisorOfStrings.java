38e4145185d4282989b4d6d9ec7498f57a45847e
khasakinil <khasakinil@gmail.com>
2024-04-24 21:55:26 +0530

    GreatestCommonDivisorOfStrings

---
+++ ProblemSolvingGeneric/src/com/dsa/generic/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,15 @@
+package com.dsa.generic;
+
+public class GreatestCommonDivisorOfStrings {
+
+	public static void main(String[] args) {
+		String str1 = "ABCABC", str2 = "ABC";
+		String str = getGreatestCommonDivisorOfStrings(str1, str2);
+		System.out.println("GreatestCommonDivisorOfStrings : " + str);
+	}
+
+	private static String getGreatestCommonDivisorOfStrings(String str1, String str2) {
+		return null;
+	}
+
+}
