59fba2aaf44c66d120185d577687a02b4e414bd6
khasakinil <khasakinil@gmail.com>
2024-04-27 18:57:14 +0530

    KidsWithTheGreatestNumberOfCandies

---
+++ ProblemSolvingGeneric/src/com/dsa/generic/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,20 @@
+package com.dsa.generic;
+/*
+ * There are n kids with candies. You are given an integer array candies, 
+ * where each candies[i] represents the number of candies the ith kid has, 
+ * and an integer extraCandies, denoting the number of extra candies that you have.
+ * Return a boolean array result of length n, where result[i] is true if, 
+ * after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+ * 
+ * Example 1:
+ * Input: candies = [2,3,5,1,3], extraCandies = 3
+ * Output: [true,true,true,false,true] 
+ */
+public class KidsWithTheGreatestNumberOfCandies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
