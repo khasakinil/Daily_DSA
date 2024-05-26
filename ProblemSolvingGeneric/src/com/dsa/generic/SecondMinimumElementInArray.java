83be798cc8e6380d3747e1a14727db2294045157
khasakinil <khasakinil@gmail.com>
2024-05-26 19:36:27 +0530

    SecondMinimumElementInArray

---
+++ ProblemSolvingGeneric/src/com/dsa/generic/SecondMinimumElementInArray.java
@@ -0,0 +1,27 @@
+package com.dsa.generic;
+
+public class SecondMinimumElementInArray {
+
+	public static void main(String[] args) {
+		int[] array = { 2, 5, 8, 4, 12, 9, 7 };
+		int secondSmallestNum = getSecondMinimumElementInArray(array);
+		System.out.println("secondSmallestNum : " + secondSmallestNum);
+	}
+
+	private static int getSecondMinimumElementInArray(int[] array) {
+		int smallestNum = Integer.MAX_VALUE;
+		int secondSmallest = Integer.MAX_VALUE;
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < smallestNum) {
+				secondSmallest = smallestNum;
+				smallestNum = array[i];
+			} else if (array[i] < secondSmallest) {
+				secondSmallest = array[i];
+			}
+		}
+
+		return secondSmallest;
+	}
+
+}
