053f6ca405f68b21384c81b2ec8aad6148d98a28
khasakinil <khasakinil@gmail.com>
2025-02-03 22:42:36 +0530

    15. 3Sum

--- ProblemSolvingGeneric/src/com/dsa/generic/apple/ThreeSum.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/apple/ThreeSum.java
@@ -1,6 +1,6 @@
 package com.dsa.generic.apple;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 15. 3Sum
@@ -33,7 +33,28 @@ public class ThreeSum {
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return new ArrayList<>();
+        }
 
-        return null;
+        Arrays.sort(nums);
+        Set<List<Integer>> result = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return new ArrayList<>(result);
     }
 }
