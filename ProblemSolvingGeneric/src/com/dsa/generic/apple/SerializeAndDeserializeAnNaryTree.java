ac9fd885ea160f02187c6743b2d617c30e2dc442
khasakinil <khasakinil@gmail.com>
2025-03-07 23:33:08 +0530

    428. Serialize and Deserialize N-ary Tree

---
+++ ProblemSolvingGeneric/src/com/dsa/generic/apple/SerializeAndDeserializeAnNaryTree.java
@@ -0,0 +1,24 @@
+package com.dsa.generic.apple;
+
+/**
+ * 428. Serialize and Deserialize N-ary Tree
+ * We need to serialize and then deserialize an N-ary tree. 
+ * Here serialization means to convert the tree structure into a string so we can store it into a file or transmit it across a network, 
+ * and then bring it back into its original structure through deserialization.
+ * For example, let us consider a 3-ary tree:
+ * 	    	1
+ *  	 /  |  \
+ *  	3   2   4
+ * 	   / \
+ * 	   5 6
+ * 
+ * We may serialize this into string: "1 3 2 4 5 6"
+ * Now, the task is to convert this string back into its original tree structure through deserialization.
+ * The problem states that there is no restriction on how the serialization/deserialization algorithm should work.
+ * The only constraints are:
+ * N lies in the range of 1 to 1000.
+ * We should not use class member/global/static variables to store states.
+ */
+public class SerializeAndDeserializeAnNaryTree {
+
+}
