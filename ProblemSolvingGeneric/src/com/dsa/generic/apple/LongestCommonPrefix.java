6a598ae0483e9bc56e115e1cbfd349e5bdf89776
khasakinil <khasakinil@gmail.com>
2025-03-27 00:19:38 +0530

    14. Longest Common Prefix

--- ProblemSolvingGeneric/src/com/dsa/generic/apple/LongestCommonPrefix.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/apple/LongestCommonPrefix.java
@@ -1,9 +1,21 @@
 package com.dsa.generic.apple;
 
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ * Example 1:
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ * Example 2:
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ */
+
 public class LongestCommonPrefix {
 
 	public static void main(String[] args) {
-		String[] strs = { "1flower", "flow", "flight" };
+		String[] strs = { "ab", "a" };
 		System.out.println("LongestCommonPrefix : " + longestCommonPrefix(strs));
 	}
 
@@ -16,6 +28,7 @@ public class LongestCommonPrefix {
 		}
 
 		String baseStr = strs[0];
+		int lastIndex = 0;
 
 		boolean isPrefEnd = false;
 		for (int i = 0; i < minStrLen; i++) {
@@ -28,8 +41,9 @@ public class LongestCommonPrefix {
 			if (isPrefEnd) {
 				return baseStr.substring(0, i);
 			}
+			lastIndex = i;
 		}
-		return baseStr;
+		return baseStr.substring(0, minStrLen);
 	}
 
 }
