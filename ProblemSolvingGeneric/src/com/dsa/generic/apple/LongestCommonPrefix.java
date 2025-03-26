4deccbea56277359ab54445e3bf1676298049e4b
khasakinil <khasakinil@gmail.com>
2025-03-27 00:19:48 +0530

    14. Longest Common Prefix

--- ProblemSolvingGeneric/src/com/dsa/generic/apple/LongestCommonPrefix.java
+++ ProblemSolvingGeneric/src/com/dsa/generic/apple/LongestCommonPrefix.java
@@ -1,6 +1,7 @@
 package com.dsa.generic.apple;
 
 /**
+ * 14. Longest Common Prefix
  * Write a function to find the longest common prefix string amongst an array of strings.
  * If there is no common prefix, return an empty string "".
  * Example 1:
